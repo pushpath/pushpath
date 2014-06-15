6cab7f2cb4afca7557e653af53331632abc28444
Richard Sentino <richard@mingdinative.com>
2014-06-16 00:20:52 +1200

    task - models for project and vm

---
+++ app/modules/project/models.ts
@@ -0,0 +1,9 @@
+export interface Project {
+    id?: string;
+    name: string;
+    box_url: string;
+    ip_address: string;
+    sync_folder: string;
+    target_folder: string;
+    hostname: string;
+}
---
+++ app/modules/settings/models.ts
@@ -0,0 +1,7 @@
+export interface VMInformation {
+    os: string;
+    cpu: string;
+    memory: string;
+    disk_size: string;
+    vm_type: string;
+}
